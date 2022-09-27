7dec4344ebf4522e59540ed523e208a8decc4e4f
unknown <dianaduya2@gmail.com>
2022-09-27 11:17:05 +0100

    Modify html and js files

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
-import data from './appData.js';
-
-const booksDisplay = document.getElementById('books-display');
-const addBook = document.getElementById('add-btn');
-const addTitle = document.getElementById('title');
-const addAuthor = document.getElementById('author');
-const removeBook = document.getElementById('remove-btn');
-const awesomeForm = document.getElementById ('awesome-form');
-
-data.forEach((data) => {
-    const {
-      title, author,
-    } = data;
-
-    // const addbtn = books.querySelector('#add-btn');
-    // addbtn.addEventListener('click', () => {
-
-    const books = document.createElement('div');
-    books.innerHTML +=`
-    <div class="book-display-container">
-        <p>${title}</p>
-        <p>${author}</p>
-        <hr>
-        <button id="remove-btn" type="button">Remove</button>
-    </div>
-`;
-booksDisplay.appendChild(books);
+// Adding books to library
+const booksDisplay = document.querySelector('#books-display');
+const titleInput = document.querySelector('#book-title');
+const authorInput = document.querySelector('#author-name');
+const addBook = document.querySelector('#add-btn');
+addBook.addEventListener("click", function(){
+    if(titleInput.value == "" && authorInput.value == ""){
+        alert("Kindly fill in the inputs");
+    }else{
+            const books = document.createElement('div');
+            const newTitle = document.createElement('h');
+            newTitle.innerHTML = titleInput.value;
+            books.appendChild(newTitle);
+            const newAuthor = document.createElement('p');
+            newAuthor.innerHTML = authorInput.value;
+            books.appendChild(newAuthor);
+            booksDisplay.appendChild(books);
+    }
 });
-// });
-
-
 // Mobile form Local Storage
 const title = form.elements.item(0);
 const author = form.elements.item(1);
@@ -39,7 +27,6 @@ function saveData() {
   };
   localStorage.setItem('data', JSON.stringify(data));
 }
-
 let formObject = JSON.parse(localStorage.getItem('data'));
 if (!formObject) {
   formObject = {
@@ -48,16 +35,13 @@ if (!formObject) {
   };
   saveData();
 }
-
 title.value = formObject.title;
 title.addEventListener('change', (e) => {
   formObject.title = e.target.value;
   localStorage.setItem('data', JSON.stringify(formObject));
 });
-
 author.value = formObject.author;
 author.addEventListener('change', (e) => {
   formObject.author = e.target.value;
   localStorage.setItem('data', JSON.stringify(formObject));
 });
-
